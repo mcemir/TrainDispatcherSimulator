7195dd6a8578317e33abe789a99b9ab4e9eea3d9
Ahmetovic Emir <a.emir.91@gmail.com>
2014-12-21 15:21:37 +0100

    Railway connections added

--- TrainDispatcherSimulator/Controls/RailwayBase.cs
+++ TrainDispatcherSimulator/Controls/RailwayBase.cs
@@ -19,9 +19,28 @@ namespace TrainDispatcherSimulator.Controls
 {
     public class RailwayBase : UserControl
     {
+        private List<RailwayBase> leftRailways = new List<RailwayBase>();
+        private List<RailwayBase> rightRailways = new List<RailwayBase>();
+
+
+
 
         #region PROPERTIES
 
+
+        // Railway name (used to display the name of the railway, e.g. "Switch1")
+        public string RailwayName
+        {
+            get { return (string)GetValue(RailwayNameProperty); }
+            set { SetValue(RailwayNameProperty, value); }
+        }
+        public static readonly DependencyProperty RailwayNameProperty =
+            DependencyProperty.Register("RailwayName", typeof(string), typeof(RailwayBase), new PropertyMetadata(""));
+
+        
+
+        
+        // The color of the railway
         public SolidColorBrush RailwayBrush
         {
             get { return (SolidColorBrush)GetValue(RailwayBrushProperty); }
@@ -32,7 +51,7 @@ namespace TrainDispatcherSimulator.Controls
 
 
 
-
+        // The platform that is bound to the railway
         public Platform Platform
         {
             get { return (Platform)GetValue(PlatformProperty); }
@@ -60,11 +79,90 @@ namespace TrainDispatcherSimulator.Controls
 
             this.MouseEnter += RailwayBase_MouseEnter;
             this.MouseLeave += RailwayBase_MouseLeave;
-        }        
+            this.Loaded += RailwayBase_Loaded;
+
+        }
+
+        void RailwayBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            populateRailwayJoints();
+        }
+
+
+        private void populateRailwayJoints()
+        {
+            // Get position
+            int row = Grid.GetRow(this);
+            int col = Grid.GetColumn(this);
+            int rowSpan = Grid.GetRowSpan(this);
+            int colSpan = Grid.GetColumnSpan(this);
+
+            Grid parentGrid = this.Parent as Grid;
+
+
+
+            leftRailways = parentGrid.Children.Cast<UserControl>().Where(c => c != this && c is RailwayBase && (
+                (Grid.GetRow(c) == row - 1 && Grid.GetColumn(c) + Grid.GetColumnSpan(c) == col && Grid.GetRowSpan(c) == 2 && !(c is RailwaySwitch4)) ||     
+                (Grid.GetRow(c) == row && Grid.GetColumn(c) + Grid.GetColumnSpan(c) == col) ||
+                (rowSpan == 2 && Grid.GetRow(c) == row + 1 && Grid.GetColumn(c) + Grid.GetColumnSpan(c) == col && !(c is RailwaySwitch3))))
+                .OrderBy(c => Grid.GetRow(c)).Cast<RailwayBase>().ToList();
+
+            leftRailways = parentGrid.Children.Cast<UserControl>().Where(c => c != this && c is RailwayBase && (
+                (Grid.GetRow(c) == row - 1 && Grid.GetColumn(c) == col + colSpan && Grid.GetRowSpan(c) == 2 && !(c is RailwaySwitch1)) ||    
+                (Grid.GetRow(c) == row && Grid.GetColumn(c) == col + colSpan) ||
+                (rowSpan == 2 && Grid.GetRow(c) == row + 1 && Grid.GetColumn(c) == col + colSpan && !(c is RailwaySwitch2))))
+                .OrderBy(c => Grid.GetRow(c)).Cast<RailwayBase>().ToList();
+
+            
+
+            // Ako je rowSpan 2 imaju specijalni slučajevi, ali ih nema na našem čvoru
+            if (rowSpan == 2 && leftRailways.Count == 1 && 
+                Grid.GetRowSpan(leftRailways[0]) == 2 &&
+                Grid.GetRow(leftRailways[0]) == row)
+            {
+                leftRailways.Add(leftRailways[0]); // Dodaj još jednu instancu
+            }
+            if (rowSpan == 2 && rightRailways.Count == 1 &&
+                Grid.GetRowSpan(rightRailways[0]) == 2 &&
+                Grid.GetRow(rightRailways[0]) == row)
+            {
+                leftRailways.Add(rightRailways[0]); // Dodaj još jednu instancu
+            }
+
+        }
 
         #endregion INITIALIZATION
 
 
+
+
+
+
+        #region PRIVATE METHODS
+
+        public virtual RailwayBase GetLeftRailway()
+        {
+            if (leftRailways.Count > 0)
+                return leftRailways[0];
+            return null;
+        }
+
+        public virtual RailwayBase GetRightRailway()
+        {
+            if (rightRailways.Count > 0)
+                return rightRailways[0];
+            return null;
+        }
+
+        #endregion PRIVATE METHODS
+
+
+
+
+
+        #region PRIVATE METHODS
+
+        #endregion PRIVATE METHODS
 
 
 
@@ -84,4 +182,6 @@ namespace TrainDispatcherSimulator.Controls
 
         #endregion EVENT HANDLERS
     }
+
+
 }
